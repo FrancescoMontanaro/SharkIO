f1925932a7bf1e704a56928738c8c36f421871fa
Francesco Montanaro <56433128+FrancescoMontanaro@users.noreply.github.com>
2021-07-06 15:29:18 +0200

    Delete Extension directory

--- Extension/Script/Paypal/returnLocal.js
+++
@@ -1,7 +0,0 @@
-function returnAskedLocal(site){
-    chrome.storage.local.get([site], function(result) {
-            var value = result[site];
-            return value;
-    });
-    console.log("sono dentro returnlocal");
-}
